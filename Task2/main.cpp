8b2339219d7c8de7305c2f5f124cf5277b5a86f0
YeEeck <yechenkai@foxmail.com>
2020-12-28 01:37:23 +0800

    finish task2 core

--- Task2/main.cpp
+++ Task2/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
 
-int safeInputInt(){
+int safeInputInt() {
     int data;
     while (true) {
         cin >> data;
@@ -11,8 +12,8 @@ int safeInputInt(){
             cin.clear();
             string str;
             cin >> str;
-            cout << "è¾“å…¥çš„ " << str << " ä¸æ˜¯ä¸€ä¸ªæœ‰æ•ˆçš„é€‰é¡¹" << endl;
-            cout << "è¯·é‡æ–°è¾“å…¥:";
+            cout << "ÊäÈëµÄ " << str << " ²»ÊÇÒ»¸öÓÐÐ§µÄÑ¡Ïî" << endl;
+            cout << "ÇëÖØÐÂÊäÈë:";
         } else {
             break;
         }
@@ -20,32 +21,175 @@ int safeInputInt(){
     return data;
 }
 
-class ALGraph{
+class ALGraph {
 private:
-    typedef struct ENode{
+    typedef struct ENode {
         int ivex;
         int weight;
         ENode *nextEdge;
-    }ENode;
+    } ENode;
 
-    typedef struct VNode{
-        int data;
-        VNode *firstEdge;
-    }VNode;
+    typedef struct VNode {
+        string data;
+        ENode *firstEdge;
+    } VNode;
 
-    VNode *mVexs;
-    int length = 1;
+    VNode mVexs[1000];
+    int length = 0;
+
+    int vis[1000] = {0};
 
 
 public:
-    ALGraph(int num){
-        mVexs = new VNode[num+1];
+    ALGraph(int num) {
+
     }
 
 
+    void addDot(const string &data) {
+//        VNode *nVNode = new VNode;
+//        nVNode->data = data;
+//        nVNode->firstEdge = NULL;
+//        mVexs[length] = *nVNode;
+        mVexs[length].data = data;
+        mVexs[length].firstEdge = NULL;
+        length++;
+    }
+
+    int addEdge(const string &p1, const string &p2, int weight) {
+        int pos1 = getPos(p1);
+        int pos2 = getPos(p2);
+        if (pos1 == -1 || pos2 == -1) {
+            return -1;
+        }
+        ENode *nENode1 = new ENode;
+        nENode1->ivex = pos2;
+        nENode1->nextEdge = NULL;
+        nENode1->weight = weight;
+        if (mVexs[pos1].firstEdge == NULL) {
+            mVexs[pos1].firstEdge = nENode1;
+        } else {
+            ENode *r = mVexs[pos1].firstEdge;
+            while (r->nextEdge) {
+                if (r->ivex == pos2) {
+                    return -2;
+                }
+                r = r->nextEdge;
+
+            }
+            r->nextEdge = nENode1;
+        }
+        ENode *nENode2 = new ENode;
+        nENode2->ivex = pos1;
+        nENode2->nextEdge = NULL;
+        nENode2->weight = weight;
+        if (mVexs[pos2].firstEdge == NULL) {
+            mVexs[pos2].firstEdge = nENode2;
+        } else {
+            ENode *r = mVexs[pos2].firstEdge;
+            while (r->nextEdge) {
+                if (r->ivex == pos1) {
+                    return -2;
+                }
+                r = r->nextEdge;
+            }
+            r->nextEdge = nENode2;
+        }
+        return 0;
+    }
+
+    int getPos(const string &data) {
+        for (int i = 0; i < length; ++i) {
+            if (mVexs[i].data == data) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    int *dfsb(int from, int to) {
+        int weight = 0;
+        int *res = new int[length]{0};
+        if (from == to) {
+            res[0] = 0;
+            res[1] = 0;
+            return res;
+        }
+        if (vis[from]) {
+            return NULL;
+        }
+        vis[from] = 1;
+
+        ENode *r = mVexs[from].firstEdge;
+        int *list;
+        res[0] = 1000000;
+        int maxList[100] = {0};
+        while (r) {
+            list = dfsb(r->ivex, to);
+            if (list == NULL) {
+                r = r->nextEdge;
+                continue;
+            }
+            if (list[0] < res[0]) {
+                res[0] = list[0];
+                memset(maxList, 0, 100);
+                for (int i = 0; i < list[1] + 2; ++i) {
+                    maxList[i] = list[i];
+                }
+                weight = r->weight;
+            }
+            r = r->nextEdge;
+        }
+        if (res[0] == 1000000) {
+            return NULL;
+        }
+        res[1] = maxList[1] + 1;
+        for (int i = 0; i < maxList[1]; ++i) {
+            res[2 + i] = maxList[2 + i];
+        }
+        res[2 + maxList[1]] = from;
+        res[0] += weight;
+        return res;
+    }
+
+    int *dfs(int from, int to) {
+        memset(vis, 0, 1000);
+        int *res = dfsb(from, to);
+        return res;
+    }
+
+    string getData(int index) {
+        return mVexs[index].data;
+    }
 };
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ALGraph alGraph(100);
+    for (int i = 1; i <= 7; ++i) {
+        alGraph.addDot(std::to_string(i));
+    }
+    alGraph.addEdge("1", "2", 2);
+    alGraph.addEdge("2", "6", 5);
+    alGraph.addEdge("6", "7", 7);
+    alGraph.addEdge("2", "5", 1);
+    alGraph.addEdge("5", "3", 2);
+    alGraph.addEdge("5", "4", 1);
+    alGraph.addEdge("4", "7", 2);
+
+    int *list = alGraph.dfs(alGraph.getPos("1"), alGraph.getPos("7"));
+    if (list == NULL) {
+        cout << "Ã»ÓÐºÏÊÊµÄÂ·Ïß" << endl;
+    } else {
+        cout << "ÕÒµ½ÏßÂ·£¬×Ü³¤Îª " << list[0] << " £¬Ðè¾­¹ý " << list[1] << " ¸öÕ¾µã" << endl;
+        for (int i = list[1] + 1; i >= 2; --i) {
+            if (i == 2){
+                cout << alGraph.getData(list[i]) << "->" << "7";
+                break;
+            }
+            cout << alGraph.getData(list[i]) << "->";
+
+        }
+        cout << endl;
+    }
     return 0;
 }
