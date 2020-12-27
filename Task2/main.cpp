8e20234f5610df4aaa837a5bbd212d8f8afe3791
YeEeck <yechenkai@foxmail.com>
2020-12-27 21:02:27 +0800

    update

--- Task2/main.cpp
+++ Task2/main.cpp
@@ -20,6 +20,31 @@ int safeInputInt(){
     return data;
 }
 
+class ALGraph{
+private:
+    typedef struct ENode{
+        int ivex;
+        int weight;
+        ENode *nextEdge;
+    }ENode;
+
+    typedef struct VNode{
+        int data;
+        VNode *firstEdge;
+    }VNode;
+
+    VNode *mVexs;
+    int length = 1;
+
+
+public:
+    ALGraph(int num){
+        mVexs = new VNode[num+1];
+    }
+
+
+};
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
