044ea063c9c3503a8e5f9bf5b5ff32fe47d203d2
YeEeck <yechenkai@foxmail.com>
2020-12-30 01:43:06 +0800

    finish

--- Task2/cmake-build-debug/cmake_install.cmake
+++ Task2/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/Code/CppProject/school-task/Task2
+# Install script for directory: E:/CPP/SchoolTask/Task2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/Code/CppProject/school-task/Task2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "E:/CPP/SchoolTask/Task2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- Task2/main.cpp
+++ Task2/main.cpp
@@ -5,11 +5,12 @@
 
 using namespace std;
 
-
+//安全输入整数(输入纠错)
 int safeInputInt() {
     int data;
     while (true) {
         cin >> data;
+        //调用cin.fail()捕获输入出错，cin.clear()清除缓存区中错误数据。
         if (cin.fail()) {
             cin.clear();
             string str;
@@ -20,60 +21,68 @@ int safeInputInt() {
             break;
         }
     }
+    //返回经过纠错的用户返回的值
     return data;
 }
 
+//图类
 class ALGraph {
 private:
+    //邻接表中表示边的结构体
     typedef struct ENode {
         int ivex;
         int weight;
         ENode *nextEdge;
     } ENode;
 
+    //邻接表中表示点的结构体
     typedef struct VNode {
         string data;
         ENode *firstEdge;
     } VNode;
 
+    //声明点数组
     VNode mVexs[1000];
     int length = 0;
-
+    //dfs防止重复路径用
     int vis[1000] = {0};
 
 
 public:
+    //构造方法，已弃用
     ALGraph(int num) {
 
     }
 
-
+    //添加点的方法
     void addDot(const string &data) {
-//        VNode *nVNode = new VNode;
-//        nVNode->data = data;
-//        nVNode->firstEdge = NULL;
-//        mVexs[length] = *nVNode;
         mVexs[length].data = data;
         mVexs[length].firstEdge = NULL;
+        //数组长度自增
         length++;
     }
 
+    //添加边的方法
     int addEdge(const string &p1, const string &p2, int weight) {
         int pos1 = getPos(p1);
         int pos2 = getPos(p2);
         if (pos1 == -1 || pos2 == -1) {
             return -1;
         }
+        //构建要新插入的值
         ENode *nENode1 = new ENode;
         nENode1->ivex = pos2;
         nENode1->nextEdge = NULL;
         nENode1->weight = weight;
+        //如果该点没有连接边则为它连接第一条边
         if (mVexs[pos1].firstEdge == NULL) {
             mVexs[pos1].firstEdge = nENode1;
         } else {
+            //如果连接了边则把新边接到结尾(顺便检查这个边是不是已经存在了)
             ENode *r = mVexs[pos1].firstEdge;
             while (r->nextEdge) {
                 if (r->ivex == pos2) {
+                    //如果边已经存在则返回错误
                     return -2;
                 }
                 r = r->nextEdge;
@@ -81,16 +90,20 @@ public:
             }
             r->nextEdge = nENode1;
         }
+        //构建要新插入的值
         ENode *nENode2 = new ENode;
         nENode2->ivex = pos1;
         nENode2->nextEdge = NULL;
         nENode2->weight = weight;
+        //如果该点没有连接边则为它连接第一条边
         if (mVexs[pos2].firstEdge == NULL) {
             mVexs[pos2].firstEdge = nENode2;
         } else {
+            //如果连接了边则把新边接到结尾(顺便检查这个边是不是已经存在了)
             ENode *r = mVexs[pos2].firstEdge;
             while (r->nextEdge) {
                 if (r->ivex == pos1) {
+                    //如果边已经存在则返回错误
                     return -2;
                 }
                 r = r->nextEdge;
@@ -100,27 +113,34 @@ public:
         return 0;
     }
 
+    //通过导航点文本获取序号的方法
     int getPos(const string &data) {
         for (int i = 0; i < length; ++i) {
             if (mVexs[i].data == data) {
+                //返回找到的序号
                 return i;
             }
         }
+        //如果没找到就返回-1
         return -1;
     }
 
-    int dp[100][100];
+//    int dp[100][100];
 
+    //dfs求路径的递归方法
     int *dfsb(int from, int to) {
         int weight = 0;
         int *res = new int[length]{0};
+        //如果找到点了就直接输入
         if (from == to) {
             res[0] = 0;
             res[1] = 0;
             return res;
         }
+        //如果这个点被访问过了就直接返回NULL退出，防止dfs回头
         if (vis[from]) {
-            return dp[from];
+//            return dp[from];
+            return NULL;
         }
         vis[from] = 1;
 
@@ -128,14 +148,19 @@ public:
         int *list;
         res[0] = 1000000;
         int maxList[100] = {0};
+        //遍历当前这个点所有的边
         while (r) {
+            //递归开始
             list = dfsb(r->ivex, to);
+            //如果返回NULL则直接退出到下一条边
             if (list == NULL || list[1] == -1) {
                 r = r->nextEdge;
                 continue;
             }
+            //list[0]存储权值之和，如果权值之和要小于目前的数据，则将目前的数据替换为刚刚返回的。
             if (list[0] < res[0]) {
                 res[0] = list[0];
+                //深拷贝
                 memset(maxList, 0, 100);
                 for (int i = 0; i < list[1] + 2; ++i) {
                     maxList[i] = list[i];
@@ -145,35 +170,43 @@ public:
             }
             r = r->nextEdge;
         }
+        //如果没有被替换说明这里三面都没有路可以走
         if (res[0] == 1000000) {
             return NULL;
         }
+        //数组的[1]存储之后的路径条数
         res[1] = maxList[1] + 1;
+        //深拷贝
         for (int i = 0; i < maxList[1]; ++i) {
             res[2 + i] = maxList[2 + i];
         }
+        //加上自己
         res[2 + maxList[1]] = from;
-        for (int i = 0; i < res[1] + 2; ++i) {
-            dp[from][i] = res[i];
-        }
+//        for (int i = 0; i < res[1] + 2; ++i) {
+//            dp[from][i] = res[i];
+//        }
         return res;
     }
 
+    //dfs入口方法，对dfs进行初始化
     int *dfs(int from, int to) {
         memset(vis, 0, 1000);
-        memset(dp, -1, 10000);
+//        memset(dp, -1, 10000);
         int *res = dfsb(from, to);
         return res;
     }
 
+    //通过序号获取序号对应的文本
     string getData(int index) {
-        if (index >= length) return "none";
+        if (index >= length) return "none";//如果序号超出范围则返回"none"
+        //返回对应序号的文本
         return mVexs[index].data;
     }
 
+    //输出所有的导航点
     void display() {
         for (int i = 0; i < length; ++i) {
-            cout << i + 1 << "." << mVexs[i].data << endl;
+            cout << i + 1 << "." << mVexs[i].data << endl;//生成菜单用，菜单从1开始，数组从0开始，所以要修改。
         }
     }
 
@@ -205,25 +238,29 @@ public:
 //        mVexs[pos].data = "";
 //    }
 
+    //数组长度，也是导航点个数获取方法
     int getLength() {
-        return length;
+        return length;//返回导航点个数
     }
 };
 
 
+//用来获取路径的函数，封装了dfs方法，加入了文本。
 void getRoad(ALGraph alGraph) {
     cout << "\n请输入你的位置:";
     int pos = safeInputInt();
     cout << "\n请输入你要到达的位置:";
     int dist1 = safeInputInt();
+    //获取最终的数据
     int *list = alGraph.dfs(pos - 1, dist1 - 1);
     if (list == NULL) {
         cout << "\n没有合适的路线" << endl;
     } else {
         cout << "\n找到线路，总长为 " << list[0] << " ，需经过 " << list[1] << " 个导航点" << endl;
+        //倒序遍历输出的才是正序的
         for (int i = list[1] + 1; i >= 2; --i) {
             if (i == 2) {
-                cout << alGraph.getData(list[i]) << "->" << alGraph.getData(dist1 - 1);
+                cout << alGraph.getData(list[i]) << "->" << alGraph.getData(dist1 - 1);//最后一个点需要手动补上
                 break;
             }
             cout << alGraph.getData(list[i]) << "->";
@@ -233,11 +270,14 @@ void getRoad(ALGraph alGraph) {
     }
 }
 
+//删除指定点的函数
 void del(const string &str) {
+    //文件操作，将文本从一个写到另一个，删除原来的，再从另一个写回去，不符合要求的不写入。
     ifstream file("data.txt", ios::in);
     ofstream tempF("temp.txt", ios::out);
     string temp;
     while (getline(file, temp)) {
+        //如果包含指定文本的直接不写入
         if (temp.find(str) != std::__cxx11::string::npos) {
             continue;
         }
@@ -258,13 +298,16 @@ void del(const string &str) {
     remove("temp.txt");
 }
 
+//添加导航点函数
 void add(ALGraph *alGraph) {
+    //同样是文件操作，直接在文件里写入对应的数据，然后重新初始化
     ifstream file("data.txt", ios::in);
     ofstream tempF("temp.txt", ios::out);
     string name;
     while (true) {
         cout << "请输入要添加的导航点的名称:";
         cin >> name;
+        //系统不允许点重名
         if (alGraph->getPos(name) == -1) {
             break;
         } else {
@@ -279,9 +322,9 @@ void add(ALGraph *alGraph) {
         tempF << endl;
     }
     while (true) {
-        cout << "\n请输入要连接到这个导航点的导航点的序号(输入-1结束):";
+        cout << "\n请输入要连接到这个导航点的导航点的序号(输入0或负数结束):";
         int num = safeInputInt();
-        if (num == -1) {
+        if (num <= 0) {
             break;
         }
         cout << "\n请输入要连接到这个导航点的导航点与这个导航点的权值:";
@@ -289,6 +332,8 @@ void add(ALGraph *alGraph) {
         if (alGraph->getData(num - 1) != "none") {
             tempF << "B " << name << " " << alGraph->getData(num - 1) << " " << weight;
             tempF << endl;
+        } else {
+            cout << "对应的导航点不存在，请重新输入" << endl;
         }
     }
     file.close();
@@ -305,10 +350,12 @@ void add(ALGraph *alGraph) {
     remove("temp.txt");
 }
 
+//主函数
 int main() {
-    first:;
+    first:;//用来重新初始化的标记
     ALGraph alGraph(100);
     //init
+    //通过文件来初始化点和边
     ifstream file("data.txt", ios::in);
     if (!file.is_open()) {
         cout << "错误:数据文件不存在" << endl;
@@ -329,6 +376,8 @@ int main() {
         }
     }
     file.close();
+
+    //以前留下的在程序里的输入点和边
 //    alGraph.addDot("大门");
 //    alGraph.addDot("图书馆");
 //    alGraph.addDot("实验楼D");
@@ -367,6 +416,7 @@ int main() {
 //    alGraph.addEdge("体育馆", "教学楼C", 100);
 //    alGraph.addEdge("体育馆", "一食堂", 10);
     //init end
+    //内部循环的菜单
     while (true) {
         cout << "------欢迎使用紫金学院导航系统------" << endl;
         cout << "导航点:" << endl;
@@ -378,6 +428,7 @@ int main() {
         cout << "D.结束程序" << endl;
         cout << "------------------------------------" << endl;
         char t;
+        //判断输入文本是否为单个字符
         while (true) {
             string str;
             cout << "请输入你要执行的操作:";
@@ -390,7 +441,7 @@ int main() {
             break;
         }
 
-        //route
+        //route 路由 对不同选项进行分流
         switch (t) {
             case 'A': {
                 getRoad(alGraph);
@@ -402,7 +453,6 @@ int main() {
             case 'B': {
                 cout << "请输入要删除的导航点序号:";
                 int pos = safeInputInt() - 1;
-                string delStr = alGraph.getData(pos);
                 if (pos < 0 || pos > alGraph.getLength()) {
                     cout << "输入的序号有误" << endl;
                     system("pause");
@@ -412,6 +462,7 @@ int main() {
                     del(alGraph.getData(pos));
                     system("pause");
                     system("cls");
+                    //写入文件之后，重新初始化
                     goto first;
                 }
             }
@@ -420,15 +471,18 @@ int main() {
                 add(&alGraph);
                 system("pause");
                 system("cls");
+                //写入文件之后，重新初始化
                 goto first;
             }
 
             case 'D': {
+                //D直接退出
                 exit(0);
             }
             default: {
                 cout << "错误的输入选项，请重新输入" << endl;
                 system("pause");
+                system("cls");
             }
         }
     }
